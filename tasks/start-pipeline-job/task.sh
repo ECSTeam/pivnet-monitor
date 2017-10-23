ee5418ca560ecbd582f08b54519da12094dbb6ae
Bridget Arthur <barthur@ecsteam.com>
2017-10-23 14:48:17 -0600

    fix fly cmd

--- tasks/start-pipeline-job/task.sh
+++ tasks/start-pipeline-job/task.sh
@@ -5,5 +5,5 @@ set -eu
 curl -k "$CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux" >> fly
 chmod +x fly
 
-fly -t concourse login -c $CONCOURSE_URL -k -u $CONCOURSE_USER -p $CONCOURSE_PWD
-fly -t concourse trigger-job -j $PIPELINE_NAME/$JOB_NAME
+./fly -t concourse login -c $CONCOURSE_URL -k -u $CONCOURSE_USER -p $CONCOURSE_PWD
+./fly -t concourse trigger-job -j $PIPELINE_NAME/$JOB_NAME
