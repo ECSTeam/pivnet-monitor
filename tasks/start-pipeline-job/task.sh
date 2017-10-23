3ba304f3644b655c603cffab711fa1a30855f832
Bridget Arthur <barthur@ecsteam.com>
2017-10-23 14:06:57 -0600

    init commit

---
+++ tasks/start-pipeline-job/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+
+curl -k $CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux
+chmod +x fly
+
+fly -t concourse login -c $CONCOURSE_URL -k -u $CONCOURSE_USER -p $CONCOURSE_PWD
+fly -t concourse trigger-job -j $PIPELINE_NAME/$JOB_NAME
