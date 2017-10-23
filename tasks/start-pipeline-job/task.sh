716fa4e6d17c1a69eaebdd261c374d2cf35a31d7
Bridget Arthur <barthur@ecsteam.com>
2017-10-23 14:46:52 -0600

    fix curl

--- tasks/start-pipeline-job/task.sh
+++ tasks/start-pipeline-job/task.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-curl -k $CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux
+curl -k "$CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux" >> fly
 chmod +x fly
 
 fly -t concourse login -c $CONCOURSE_URL -k -u $CONCOURSE_USER -p $CONCOURSE_PWD
